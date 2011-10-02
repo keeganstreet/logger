2cffd1c3de9d9977ecaa6314f5e9b080b2426756
Keegan Street <keeganstreet@gmail.com>
2011-10-02 17:39:26 +0200

    Added a button to toggle the introduction panel

--- public/js/logger.js
+++ public/js/logger.js
@@ -1,11 +1,26 @@
 var logger = (function(module) {
   var socket = io.connect('http://logger.nodester.com'),
-    $logs = $('#logs tbody');
+    $logs = $('#logs tbody'),
+    introVisible = true,
+    $intro = $('#introduction'),
+    $toggleIntro = $('#toggleIntro');
 
   socket.on('update', function (entry) {
-    console.log(entry);
     var $row = $('<tr><td>' + entry.date.toString() + '</td><td>' + entry.project + '</td><td>' + entry.windowLocation + '</td><td>' + entry.file + '</td><td>' + entry.line + '</td><td>' + entry.message + '</td><td>' + entry.userAgent + '</td></tr>');
     $logs.prepend($row);
+  });
+
+  $toggleIntro.click(function(e) {
+    e.preventDefault();
+    if (introVisible) {
+      introVisible = false;
+      $intro.slideUp();
+      $toggleIntro.text('Show introduction');
+    } else {
+      introVisible = true;
+      $intro.slideDown();
+      $toggleIntro.text('Hide introduction');
+    }
   });
 
   return module;
