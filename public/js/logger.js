43376d3fe54c06c0461d76fddff3db50dabcd4b8
Keegan Street <keeganstreet@gmail.com>
2011-10-02 17:54:32 +0200

    Added localStorage to remember if the intro panel is open or closed. P.S. I'm 10,000m in the air

--- public/js/logger.js
+++ public/js/logger.js
@@ -1,3 +1,5 @@
+/*global Modernizr, io */
+
 var logger = (function(module) {
   var socket = io.connect('http://logger.nodester.com'),
     $logs = $('#logs tbody'),
@@ -5,6 +7,19 @@ var logger = (function(module) {
     $intro = $('#introduction'),
     $toggleIntro = $('#toggleIntro');
 
+  // Retrieve previous state of the introduction
+  if (Modernizr.localstorage && typeof localStorage.introVisible !== 'undefined') {
+    introVisible = (localStorage.introVisible === 'true' ? true : false);
+  }
+  if (introVisible) {
+    $intro.show();
+    $toggleIntro.text('Hide introduction');
+  } else {
+    $intro.hide();
+    $toggleIntro.text('Show introduction');
+  }
+
+  // Update the log list when we get a web socket
   socket.on('update', function (entry) {
     var $row = $('<tr><td>' + entry.date.toString() + '</td><td>' + entry.project + '</td><td>' + entry.windowLocation + '</td><td>' + entry.file + '</td><td>' + entry.line + '</td><td>' + entry.message + '</td><td>' + entry.userAgent + '</td></tr>');
     $logs.prepend($row);
@@ -20,6 +35,9 @@ var logger = (function(module) {
       introVisible = true;
       $intro.slideDown();
       $toggleIntro.text('Hide introduction');
+    }
+    if (Modernizr.localstorage) {
+      localStorage.introVisible = introVisible;
     }
   });
 
